81072202f633fd67eff37671c6714607d5d16ee8
Eric Brincheski <ericbrincheskie@gmail.com>
2024-12-30 17:52:36 -0600

    add circle shape class

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def main():
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
